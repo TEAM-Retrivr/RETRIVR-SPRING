5d85f4c4573c819e8c0be1e61a454fe4bac440e1
chanwookK <kan0202@naver.com>
2026-02-11 03:49:22 +0900

    Init: Initial Commit

---
+++ src/main/java/retrivr/retrivrspring/RetrivrSpringApplication.java
@@ -0,0 +1,13 @@
+package retrivr.retrivrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrivrSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RetrivrSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/retrivr/retrivrspring/RetrivrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package retrivr.retrivrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrivrSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
