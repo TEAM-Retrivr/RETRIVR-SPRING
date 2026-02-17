d849d73f77db5436a03e0e43deb199e642af95a1
kangchanwook <kan0202@naver.com>
2026-02-17 20:45:35 +0900

    feat: [RTR-48] [대여지 검색] Swagger 문서화 및 Controller 구현. Response DTO 변경

--- src/main/java/retrivr/retrivrspring/presentation/organization/PublicOrganizationSearchController.java
+++ src/main/java/retrivr/retrivrspring/presentation/organization/PublicOrganizationSearchController.java
@@ -8,17 +8,23 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import retrivr.retrivrspring.application.service.organization.OrganizationSearchService;
+import retrivr.retrivrspring.global.error.ErrorCode;
+import retrivr.retrivrspring.global.swagger.annotation.ApiErrorCodeExamples;
 import retrivr.retrivrspring.presentation.organization.res.OrganizationSearchPageResponse;
-import retrivr.retrivrspring.presentation.organization.res.OrganizationSearchPageResponse.OrganizationSearchSummary;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/public/v1/organizations/search")
 @Tag(name = "Public API / Organization", description = "")
 public class PublicOrganizationSearchController {
+
+  private final OrganizationSearchService organizationSearchService;
 
   @GetMapping
   @Operation(summary = "대여지 검색")
@@ -26,20 +32,16 @@ public class PublicOrganizationSearchController {
       responseCode = "200",
       description = "검색 결과",
       content = @Content(schema = @Schema(implementation = OrganizationSearchPageResponse.class)))
-  public OrganizationSearchPageResponse search(
+  @ApiErrorCodeExamples({ErrorCode.NO_SEARCH_KEYWORD_EXCEPTION, ErrorCode.BLANK_SEARCH_KEYWORD_EXCEPTION, ErrorCode.DO_NOT_ENCODED_SEARCH_CURSOR, ErrorCode.INVALID_SEARCH_CURSOR})
+  public OrganizationSearchPageResponse searchRankedPageByKeyword(
+      @Parameter(description = "검색 키워드" , example = "건국대학교 도서관자치위원회")
       @RequestParam(name = "keyword") String keyword,
-      @Parameter(description = "커서(마지막 조회된 orgId). 다음 페이지 조회 시 사용", example = "2")
+      @Parameter(description = "인코딩된 복합 커서. 다음 페이지 조회 시 사용. null 일 경우 첫번째 페이지 조회", example = "eyJidWNrZXQiOjMsInNpbSI6MC4zMTgxODIsIm9yZ2FuaXphdGlvbklkIjo1fQ")
       @RequestParam(name = "cursor", required = false)
-      Long cursor,
+      String cursor,
       @RequestParam(name = "size", required = false, defaultValue = "15") Integer size
   ) {
-    return new OrganizationSearchPageResponse(
-        List.of(
-          new OrganizationSearchSummary(1L, "건국대학교 도서관자치위원회", "https://s3.northeast/image/dojawi"),
-          new OrganizationSearchSummary(2L, "건국대학교 동아리연합회", "https://s3.northeast/image/clubunion")
-        ),
-        2L
-    );
+    return organizationSearchService.searchRankedPageByKeyword(keyword, cursor, size);
   }
 
   @GetMapping("/suggestions")
