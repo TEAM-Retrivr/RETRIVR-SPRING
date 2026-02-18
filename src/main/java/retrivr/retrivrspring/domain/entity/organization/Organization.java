48d2398bf4f2214f7d054c6e2dc40e22e1a3658a
dasol <pds2023@gmail.com>
2026-02-19 02:07:58 +0900

    feat: [RTR-53] Admin login API OrganizationRepository 구현

--- src/main/java/retrivr/retrivrspring/domain/entity/organization/Organization.java
+++ src/main/java/retrivr/retrivrspring/domain/entity/organization/Organization.java
@@ -1,20 +1,10 @@
 package retrivr.retrivrspring.domain.entity.organization;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import retrivr.retrivrspring.domain.entity.BaseTimeEntity;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -47,4 +37,8 @@ public class Organization extends BaseTimeEntity {
 
   @Column(name = "search_key", unique = true, length = 255)
   private String searchKey;
+
+  public void updateLastLoginAt(LocalDateTime time) {
+    this.lastLoginAt = time;
+  }
 }
