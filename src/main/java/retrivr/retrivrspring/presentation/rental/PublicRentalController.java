04377b6da46b293fdb5689e3658a15af0d90b62c
kangchanwook <kan0202@naver.com>
2026-02-20 17:55:07 +0900

    feat: [RTR-48] [대여 요청/조회] Presentation 레이어 구현

--- src/main/java/retrivr/retrivrspring/presentation/rental/PublicRentalController.java
+++ src/main/java/retrivr/retrivrspring/presentation/rental/PublicRentalController.java
@@ -5,23 +5,27 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import retrivr.retrivrspring.domain.entity.rental.enumerate.RentalStatus;
+import retrivr.retrivrspring.application.service.rental.PublicRentalService;
+import retrivr.retrivrspring.global.error.ErrorCode;
+import retrivr.retrivrspring.global.swagger.annotation.ApiErrorCodeExamples;
 import retrivr.retrivrspring.presentation.rental.req.PublicRentalCreateRequest;
 import retrivr.retrivrspring.presentation.rental.res.PublicRentalCreateResponse;
 import retrivr.retrivrspring.presentation.rental.res.PublicRentalDetailResponse;
 
+@RequiredArgsConstructor
 @RestController
 @Tag(name = "Public Rental API", description = "대여자용 대여 요청/상태 조회")
 @RequestMapping("/api/public/v1")
 public class PublicRentalController {
+
+  private final PublicRentalService publicRentalService;
 
   @PostMapping("/items/{itemId}/rentals")
   @Operation(summary = "대여 요청 생성(대여자 정보 입력 + 요청)")
@@ -30,16 +34,12 @@ public class PublicRentalController {
       description = "대여 요청 생성 성공",
       content = @Content(schema = @Schema(implementation = PublicRentalCreateResponse.class))
   )
+  @ApiErrorCodeExamples({ErrorCode.NOT_FOUND_ITEM, ErrorCode.NOT_FOUND_ITEM_UNIT, ErrorCode.ILLEGAL_BORROWER_FIELD})
   public PublicRentalCreateResponse createRental(
       @PathVariable("itemId") Long itemId,
       @RequestBody PublicRentalCreateRequest request
   ) {
-    return new PublicRentalCreateResponse(
-        1001L,
-        itemId,
-        request.itemUnitId(),
-        LocalDateTime.now()
-    );
+    return publicRentalService.requestRental(itemId, request);
   }
 
   @GetMapping("/rentals/{rentalId}")
@@ -49,20 +49,11 @@ public class PublicRentalController {
       description = "대여 정보 조회 성공",
       content = @Content(schema = @Schema(implementation = PublicRentalDetailResponse.class))
   )
+  @ApiErrorCodeExamples({ErrorCode.NOT_FOUND_RENTAL})
   public PublicRentalDetailResponse getRentalInfo(
       @PathVariable("rentalId") Long rentalId
   ) {
-    return new PublicRentalDetailResponse(
-      rentalId,
-        RentalStatus.APPROVED,
-        "C타입 충전기",
-        "c타입 충전기(1)",
-        "조윤아",
-        "동물자원과학과",
-        "202312690",
-        LocalDate.now(),
-        LocalDate.now().plusDays(2)
-    );
+    return publicRentalService.checkRentalStatusAndDetail(rentalId);
   }
 
 }
--- src/main/java/retrivr/retrivrspring/presentation/rental/res/PublicRentalDetailResponse.java
+++ src/main/java/retrivr/retrivrspring/presentation/rental/res/PublicRentalDetailResponse.java
@@ -1,6 +1,10 @@
 package retrivr.retrivrspring.presentation.rental.res;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import retrivr.retrivrspring.domain.entity.rental.Borrower;
+import retrivr.retrivrspring.domain.entity.rental.Rental;
 import retrivr.retrivrspring.domain.entity.rental.enumerate.RentalStatus;
 
 public record PublicRentalDetailResponse(
@@ -8,11 +12,21 @@ public record PublicRentalDetailResponse(
     RentalStatus rentalStatus,
     String itemName,
     String itemUnitCode,
-    String borrowerName,
-    String borrowerMajor,
-    String borrowerStudentNumber,
-    LocalDate rentalDate,
-    LocalDate returnDueDate
+    Map<String, String> borrowerField,
+    LocalDateTime decidedAt,
+    LocalDate dueDate
 ) {
+
+  public static PublicRentalDetailResponse from(Rental rental, String itemName, String itemUnitCode, Map<String, String> borrowerField) {
+    return new PublicRentalDetailResponse(
+        rental.getId(),
+        rental.getStatus(),
+        itemName,
+        itemUnitCode,
+        borrowerField,
+        rental.getDecidedAt(),
+        rental.getDueDate()
+    );
+  }
 
 }
