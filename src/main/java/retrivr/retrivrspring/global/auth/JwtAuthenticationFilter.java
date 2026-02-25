92cf144eeca85eb44e3f4ee7d921f2da91513391
dasol <pds2023@gmail.com>
2026-02-25 15:36:40 +0900

    feat: [RTR-92] JwtAuthenticationFilter 추가

---
+++ src/main/java/retrivr/retrivrspring/global/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,37 @@
+package retrivr.retrivrspring.global.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+import retrivr.retrivrspring.global.config.JwtTokenProvider;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String bearerToken = request.getHeader("Authorization");
+        String token = jwtTokenProvider.resolveToken(bearerToken);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+
+            AuthUser authUser = jwtTokenProvider.getAuthUser(token);
+
+            request.setAttribute("authUser", authUser);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
