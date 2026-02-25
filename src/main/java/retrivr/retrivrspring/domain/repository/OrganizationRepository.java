4007ec68d7bbd2a52161f6eeb1d60ca13c8efc3c
dasol <pds2023@gmail.com>
2026-02-25 23:57:55 +0900

    chore: 코드 리펙토링 과정 중 코드 순서 변경

--- src/main/java/retrivr/retrivrspring/domain/repository/OrganizationRepository.java
+++ src/main/java/retrivr/retrivrspring/domain/repository/OrganizationRepository.java
@@ -2,11 +2,11 @@ package retrivr.retrivrspring.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import retrivr.retrivrspring.domain.entity.organization.Organization;
-
-import java.util.Optional;
 import retrivr.retrivrspring.infrastructure.repository.organization.OrganizationSearchRepository;
 
+import java.util.Optional;
+
 public interface OrganizationRepository extends JpaRepository<Organization, Long>,
-    OrganizationSearchRepository {
+        OrganizationSearchRepository {
     Optional<Organization> findByEmail(String email);
 }
