4c201efe66b780bca7ec38285e80257a38d49ee2
dasol <pds2023@gmail.com>
2026-02-25 15:37:42 +0900

    feat: [RTR-92] WebConfig에 AuthUserArgumentResolver 등록

---
+++ src/main/java/retrivr/retrivrspring/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package retrivr.retrivrspring.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import retrivr.retrivrspring.global.auth.AuthUserArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+}
