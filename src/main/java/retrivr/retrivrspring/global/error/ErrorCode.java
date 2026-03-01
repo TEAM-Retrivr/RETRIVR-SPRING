b58ebfa2e880a841089cd915e6a852d60bfa4ced
chanwookK <kan0202@naver.com>
2026-03-02 01:18:48 +0900

    fix: [RTR-50] 에러 코드 수정

--- src/main/java/retrivr/retrivrspring/global/error/ErrorCode.java
+++ src/main/java/retrivr/retrivrspring/global/error/ErrorCode.java
@@ -48,7 +48,7 @@ public enum ErrorCode {
     NOT_FOUND_RENTAL(HttpStatus.NOT_FOUND, 5001, "존재하지 않는 대여 정보입니다."),
     RENTAL_STATUS_TRANSITION_EXCEPTION(HttpStatus.BAD_REQUEST, 5002, "대여 상태를 요청된 상태로 변경할 수 없습니다."),
     INVALID_RENTAL_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, 5003, "대여 데이터가 잘못되었습니다."),
-    RENTAL_DUE_DATE_UPDATE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, 5004, "반납 일자를 수정할 수 없는 상태입니다."),
+    RENTAL_DUE_DATE_UPDATE_EXCEPTION(HttpStatus.BAD_REQUEST, 5004, "반납 일자를 수정할 수 없는 상태입니다."),
 
     // 6000: Authentication Error
     INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, 6000, "이메일 또는 비밀번호가 올바르지 않습니다."),
@@ -83,6 +83,9 @@ public enum ErrorCode {
     PASSWORD_RESET_TOKEN_ALREADY_USED(HttpStatus.BAD_REQUEST, 8003, "이미 사용된 비밀번호 재설정 토큰입니다."),
     PASSWORD_RESET_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, 8004, "비밀번호와 비밀번호 확인 값이 일치하지 않습니다."),
     PASSWORD_RESET_POLICY_VIOLATION(HttpStatus.BAD_REQUEST, 8005, "비밀번호 정책을 만족하지 않습니다."),
+
+    // 9000: Return Event Error
+    RETURN_EVENT_CAN_NOT_CREATE(HttpStatus.INTERNAL_SERVER_ERROR, 9000, "반납 이벤트를 생성할 수 없습니다.");
     ;
 
     private final HttpStatus httpStatus;
