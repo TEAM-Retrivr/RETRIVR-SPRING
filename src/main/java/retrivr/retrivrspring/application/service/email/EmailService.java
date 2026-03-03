4dcb171b269ab259e40143b3aed3377ad9a4fbd2
dasol <pds2023@gmail.com>
2026-03-04 01:20:49 +0900

    refactor:[RTR-110] EmailService 미사용 메서드 삭제

--- src/main/java/retrivr/retrivrspring/application/service/email/EmailService.java
+++ src/main/java/retrivr/retrivrspring/application/service/email/EmailService.java
@@ -2,7 +2,5 @@ package retrivr.retrivrspring.application.service.email;
 
 public interface EmailService {
 
-    void sendTextEmail(String toEmail, String subject, String body);
-
     void sendHtmlEmail(String toEmail, String subject, String htmlBody);
 }
--- src/main/java/retrivr/retrivrspring/infrastructure/email/EmailServiceImpl.java
+++ src/main/java/retrivr/retrivrspring/infrastructure/email/EmailServiceImpl.java
@@ -1,18 +1,16 @@
 package retrivr.retrivrspring.infrastructure.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import retrivr.retrivrspring.application.service.email.EmailService;
 import retrivr.retrivrspring.global.error.ApplicationException;
 import retrivr.retrivrspring.global.error.ErrorCode;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 
 @Component
 @RequiredArgsConstructor
@@ -22,21 +20,6 @@ public class EmailServiceImpl implements EmailService {
 
     @Value("${spring.mail.username:no-reply@retrivr.com}")
     private String fromAddress;
-
-    @Override
-    public void sendTextEmail(String toEmail, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromAddress);
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-
-        try {
-            mailSender.send(message);
-        } catch (MailException ex) {
-            throw new ApplicationException(ErrorCode.INTERNAL_SERVER_EXCEPTION);
-        }
-    }
 
     @Override
     public void sendHtmlEmail(String toEmail, String subject, String htmlBody) {
