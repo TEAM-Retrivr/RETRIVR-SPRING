6d69afb64bd26b95e472679d6694c3a28d75434f
dasol <pds2023@gmail.com>
2026-03-12 01:12:44 +0900

    feat:[RTR-55] item unit repository에 organization scoped 조회 메서드 추가

--- src/main/java/retrivr/retrivrspring/domain/repository/item/ItemUnitRepository.java
+++ src/main/java/retrivr/retrivrspring/domain/repository/item/ItemUnitRepository.java
@@ -2,6 +2,7 @@ package retrivr.retrivrspring.domain.repository.item;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import retrivr.retrivrspring.domain.entity.item.Item;
 import retrivr.retrivrspring.domain.entity.item.ItemUnit;
 import retrivr.retrivrspring.infrastructure.repository.item.ItemUnitLookupRepository;
 
@@ -9,4 +10,11 @@ public interface ItemUnitRepository extends JpaRepository<ItemUnit, Long>,
     ItemUnitLookupRepository {
 
   List<ItemUnit> findAllByItemId(Long itemId);
+
+  void deleteByItem(Item item);
+
+  boolean existsByItemIdAndCode(Long itemId, String code);
+
+  java.util.Optional<ItemUnit> findByIdAndItemIdAndItemOrganizationId(Long itemUnitId, Long itemId,
+      Long organizationId);
 }
