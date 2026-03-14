829ba704567906cd127f63cd5f4a3ca803493e35
dasol <pds2023@gmail.com>
2026-03-14 22:15:47 +0900

    fix:[RTR-151] AdminItemUpdateResponse 컬럼 변경

--- src/main/java/retrivr/retrivrspring/presentation/admin/item/res/AdminItemUpdateResponse.java
+++ src/main/java/retrivr/retrivrspring/presentation/admin/item/res/AdminItemUpdateResponse.java
@@ -1,30 +1,50 @@
 package retrivr.retrivrspring.presentation.admin.item.res;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.List;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import retrivr.retrivrspring.domain.entity.item.Item;
 import retrivr.retrivrspring.domain.entity.item.ItemBorrowerField;
 import retrivr.retrivrspring.domain.entity.item.enumerate.ItemManagementType;
+
+import java.util.List;
 
 @Schema(description = "관리자 물품 수정 응답")
 public record AdminItemUpdateResponse(
     @Schema(description = "물품 ID", example = "12")
     Long itemId,
 
-    @Schema(description = "물품명", example = "C타입 고속 충전기")
+    @Schema(description = "물품명", example = "C타입 충전기")
+    @NotBlank
+    @Size(max = 255)
     String name,
 
     @Schema(description = "물품 설명", example = "고속 충전 지원 충전기")
+    @Size(max = 2000)
     String description,
 
-    @Schema(description = "대여 가능 기간(일)", example = "5")
+    @Schema(description = "대여 가능 기간(일)", example = "3")
+    @NotNull
+    @Positive
     Integer rentalDuration,
 
-    @Schema(description = "활성 여부", example = "true")
-    Boolean isActive,
+    @Schema(description = "물품 총 개수(개)", example = "3")
+    @NotNull
+    @Positive
+    Integer totalQuantity,
 
-    @Schema(description = "물품 관리 방식", example = "UNIT")
+    @Schema(description = "물품 관리 방식", example = "NON_UNIT")
+    @NotNull
     ItemManagementType itemManagementType,
+
+    @Schema(description = "독촉 문자 발송 여부", example = "true")
+    @NotNull
+    Boolean useMessageAlarmService,
+
+    @Schema(description = "담보물품 여부 및 종류", example = "학생증")
+    String guaranteedGoods,
 
     @Schema(description = "대여자 입력 요구 정보 목록")
     List<BorrowerRequirementResponse> borrowerRequirements
@@ -36,8 +56,10 @@ public record AdminItemUpdateResponse(
         item.getName(),
         item.getDescription(),
         item.getRentalDuration(),
-        item.isActive(),
+        item.getTotalQuantity(),
         item.getItemManagementType(),
+        item.isUseMessageAlarmService(),
+        item.getGuaranteedGoods(),
         borrowerFields.stream()
             .map(BorrowerRequirementResponse::from)
             .toList()
