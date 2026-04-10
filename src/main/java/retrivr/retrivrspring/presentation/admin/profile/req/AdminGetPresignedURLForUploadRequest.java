0ee8cd58dd14bf792a33ca3b7cba4244f52b814b
kangchanwook <kan0202@naver.com>
2026-04-10 15:38:17 +0900

    fix: [RTR-195] swagger schema 오타 수정

--- src/main/java/retrivr/retrivrspring/presentation/admin/profile/req/AdminGetPresignedURLForUploadRequest.java
+++ src/main/java/retrivr/retrivrspring/presentation/admin/profile/req/AdminGetPresignedURLForUploadRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record AdminGetPresignedURLForUploadRequest(
-    @Schema(description = "변경할 이미지 컨텐츠 타입. (허용목록: image/jpeg, image/jpg, image/png, image/webp", example = "image/jpg")
+    @Schema(description = "변경할 이미지 컨텐츠 타입. (허용목록: image/jpeg, image/jpg, image/png, image/webp)", example = "image/jpg")
     @NotNull
     String imageContentType
 ) {
