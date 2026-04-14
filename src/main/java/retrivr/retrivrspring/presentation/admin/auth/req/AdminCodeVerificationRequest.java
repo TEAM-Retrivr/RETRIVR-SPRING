722ea7c38123cf0449f5dc47f5d27486c40c8a13
kangchanwook <kan0202@naver.com>
2026-04-14 21:18:23 +0900

    fix: [RTR-175] AdminCodeVerificationRequest - adminCode 에 NotNull 제약 추가

--- src/main/java/retrivr/retrivrspring/presentation/admin/auth/req/AdminCodeVerificationRequest.java
+++ src/main/java/retrivr/retrivrspring/presentation/admin/auth/req/AdminCodeVerificationRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Pattern;
 import retrivr.retrivrspring.domain.entity.organization.enumerate.AdminCodeVerificationPurpose;
 
 public record AdminCodeVerificationRequest(
+    @NotNull
     @Pattern(regexp = "\\d{6}", message = "관리자 코드는 6자리 숫자여야 합니다.")
     String adminCode,
     @NotNull
