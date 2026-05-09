841e8565e330a5f0c19c558acdbc63bbb6748531
kangchanwook <kan0202@naver.com>
2026-05-10 01:06:30 +0900

    feat: [RTR-256] 이용권 생성, 검증 서비스 구현

---
+++ src/main/java/retrivr/retrivrspring/application/service/admin/membership/pass/MembershipPassService.java
@@ -0,0 +1,139 @@
+package retrivr.retrivrspring.application.service.admin.membership.pass;
+
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import retrivr.retrivrspring.domain.entity.membership.CouponRegistration;
+import retrivr.retrivrspring.domain.entity.membership.MembershipPass;
+import retrivr.retrivrspring.domain.entity.membership.Subscription;
+import retrivr.retrivrspring.domain.entity.membership.enumerate.MembershipLevel;
+import retrivr.retrivrspring.domain.entity.membership.enumerate.MembershipPassStatus;
+import retrivr.retrivrspring.domain.entity.organization.Organization;
+import retrivr.retrivrspring.domain.repository.membership.pass.MembershipPassRepository;
+import retrivr.retrivrspring.domain.repository.organization.OrganizationRepository;
+import retrivr.retrivrspring.global.error.ApplicationException;
+import retrivr.retrivrspring.global.error.ErrorCode;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MembershipPassService {
+
+  private final OrganizationRepository organizationRepository;
+  private final MembershipPassRepository membershipPassRepository;
+
+  @Transactional
+  public MembershipPass generateCouponMembershipPass(Long loginOrganizationId,
+      CouponRegistration couponRegistration) {
+    LocalDateTime now = LocalDateTime.now();
+
+    Organization organization = organizationRepository.findById(loginOrganizationId)
+        .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_ORGANIZATION));
+
+    couponRegistration.validateOwner(organization);
+
+    MembershipPass lastPass = membershipPassRepository.findFirstByOrganizationOrderBySequenceDesc(
+            organization)
+        .orElse(null);
+
+    LocalDateTime startAt;
+    long sequence;
+    if (lastPass == null) {
+      startAt = now;
+      sequence = 1L;
+    } else {
+      startAt = lastPass.getEndAt().isBefore(now)
+          ? now
+          : lastPass.getEndAt();
+
+      sequence = lastPass.getSequence() + 1;
+    }
+
+    MembershipPass pass = MembershipPass.createCouponPass(
+        organization,
+        MembershipLevel.PREMIUM,
+        couponRegistration,
+        startAt,
+        couponRegistration.getCoupon().getDurationDays(),
+        sequence
+    );
+
+    if (pass.isActivable(now)) {
+      pass.activate();
+    }
+
+    try {
+      return membershipPassRepository.saveAndFlush(pass);
+    } catch (DataIntegrityViolationException e) {
+      throw new ApplicationException(ErrorCode.MEMBERSHIP_PASS_CONFLICT);
+    }
+  }
+
+  @Transactional
+  public MembershipPass generateSubscriptionMembershipPass(Long loginOrganizationId,
+      Subscription subscription) {
+    LocalDateTime now = LocalDateTime.now();
+
+    Organization organization = organizationRepository.findById(loginOrganizationId)
+        .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_ORGANIZATION));
+
+    subscription.validateOwner(organization);
+
+    MembershipPass lastPass = membershipPassRepository.findFirstByOrganizationOrderBySequenceDesc(
+            organization)
+        .orElse(null);
+
+    LocalDateTime startAt;
+    long sequence;
+    if (lastPass == null) {
+      startAt = now;
+      sequence = 1L;
+    } else {
+      startAt = lastPass.getEndAt().isBefore(now)
+          ? now
+          : lastPass.getEndAt();
+
+      sequence = lastPass.getSequence() + 1;
+    }
+
+    MembershipPass pass = MembershipPass.createSubscriptionPass(
+        organization,
+        MembershipLevel.PREMIUM,
+        subscription,
+        startAt,
+        subscription.getDurationDays(),
+        sequence
+    );
+
+    if (pass.isActivable(now)) {
+      pass.activate();
+    }
+
+    try {
+      return membershipPassRepository.saveAndFlush(pass);
+    } catch (DataIntegrityViolationException e) {
+      throw new ApplicationException(ErrorCode.MEMBERSHIP_PASS_CONFLICT);
+    }
+  }
+
+  public MembershipLevel validateMembershipPass(Long organizationId, LocalDateTime now) {
+    Organization organization = organizationRepository.findById(organizationId)
+        .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_ORGANIZATION));
+
+    MembershipPass membershipPass = membershipPassRepository.findFirstByOrganizationAndStatusOrderBySequenceDesc(
+            organization, MembershipPassStatus.ACTIVE)
+        .orElse(null);
+
+    if (membershipPass == null) {
+      return MembershipLevel.FREE;
+    }
+
+    if (membershipPass.isExpired(now)) {
+      return MembershipLevel.FREE;
+    }
+
+    return membershipPass.getLevel();
+  }
+}
