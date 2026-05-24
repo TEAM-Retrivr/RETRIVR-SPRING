a670431f4fc962b058fa3d6cebb2bd5bad942fa8
dasol <pds2023@gmail.com>
2026-05-24 19:35:17 +0900

    feat:[RTR-128] 리프레시 토큰 저장소와 화이트리스트 스키마 추가

---
+++ src/main/java/retrivr/retrivrspring/domain/repository/auth/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package retrivr.retrivrspring.domain.repository.auth;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import retrivr.retrivrspring.domain.entity.organization.RefreshToken;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByTokenValue(String tokenValue);
+
+    void deleteByTokenValue(String tokenValue);
+}
