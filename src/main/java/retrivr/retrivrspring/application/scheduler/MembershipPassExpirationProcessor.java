12fbe32e26276b0f7d9f6d18fd0205fbf0168a05
kangchanwook <kan0202@naver.com>
2026-08-04 14:29:09 +0900

    chore: [RTR-257] 멤버십 패스 만료 스케줄러 활성화 및 로깅

--- src/main/java/retrivr/retrivrspring/application/scheduler/MembershipPassExpirationProcessor.java
+++ src/main/java/retrivr/retrivrspring/application/scheduler/MembershipPassExpirationProcessor.java
@@ -3,6 +3,7 @@ package retrivr.retrivrspring.application.scheduler;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import retrivr.retrivrspring.application.service.admin.membership.pass.MembershipPassExpirationService;
@@ -10,6 +11,7 @@ import retrivr.retrivrspring.domain.entity.membership.MembershipPass;
 import retrivr.retrivrspring.domain.entity.membership.enumerate.MembershipPassStatus;
 import retrivr.retrivrspring.domain.repository.membership.pass.MembershipPassRepository;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MembershipPassExpirationProcessor {
@@ -27,8 +29,14 @@ public class MembershipPassExpirationProcessor {
             now
         );
 
+    int expiredCount = 0;
     for (MembershipPass expiredPass : expiredPasses) {
       membershipPassExpirationService.processExpiredPass(expiredPass, now);
+      expiredCount++;
+    }
+
+    if (expiredCount > 0) {
+      log.info("Expired {} membership passes", expiredCount);
     }
   }
 }
