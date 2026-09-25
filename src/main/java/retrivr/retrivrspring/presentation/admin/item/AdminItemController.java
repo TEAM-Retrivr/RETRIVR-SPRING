f314093263763687075de5a61a32466346c1ab1e
dasol <pds2023@gmail.com>
2026-09-25 21:43:02 +0900

    docs: 물품 수정 API에 유닛 미존재 오류 응답 추가

--- src/main/java/retrivr/retrivrspring/presentation/admin/item/AdminItemController.java
+++ src/main/java/retrivr/retrivrspring/presentation/admin/item/AdminItemController.java
@@ -106,6 +106,7 @@ public class AdminItemController {
   @ApiErrorCodeExamples({
       ErrorCode.NOT_FOUND_ORGANIZATION,
       ErrorCode.NOT_FOUND_ITEM,
+      ErrorCode.NOT_FOUND_ITEM_UNIT,
       ErrorCode.BAD_REQUEST_EXCEPTION,
       ErrorCode.ITEM_UNIT_DELETE_WITH_ACTIVE_RENTAL,
       ErrorCode.DUPLICATE_ITEM_UNIT_LABEL,
